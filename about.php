a0c468da31d775fbe40f32d8635de6f15378a9a7
Iqbalemaulana03 <82009410+Iqbalemaulana03@users.noreply.github.com>
2021-06-20 06:58:04 +0700

    Update about.php

--- about.php
+++ about.php
@@ -6,50 +6,15 @@
 </head>
 <body>
     <?= $this->include('template/header'); ?>
-    <div class="about-section">
-        <h1 style ="text-align:center">ABOUT US</h1>
-
-            <p style ="text-align:center"> <b>NAMA  : SYUKUR YAKUB</b></p>
-            <p style ="text-align:center"> <b>NIM   : 311910696</b></p>
-            <p style ="text-align:center"> <b>KELAS : TI.19.C1</b></p>
-            <p style ="text-align:center"> <b><i>from Pelita Bangsa University</i></b></p>
-
-        <h2 style = "text-align:center">LOGO UNIVERSITAS PELITA BANGSA</h2>
-            <img class = "logo" src="logo_upb.png" width="200" >
-            <style>
-                h2
-                {
-                    background-color: yellow;
-                    color: rgb(20, 57, 220);
-                    font-family: sans-serif;
-                    text-align: center;
-                    width: 45%;
-                    margin:auto;
-                    padding: 10px;
-                }  
-            </style> 
-    </div>
-    <h3>DESKRIPSI TENTANG UNIVERSITAS PELITA BANGSA</h3>
-    <style>
-        h3
-        {
-            background-color: yellow;
-            color: rgb(20, 57, 220);
-            font-family: sans-serif;
-            text-align: center;
-            width: 100%;
-            margin:auto;
-            padding: 10px;
-        }  
-    </style>
-    <p3><i>Pelita Bangsa (UPB) merupakan perguruan tinggi baru hasil perubahan 
-    bentuk dari penggabungan antara Sekolah Tinggi Ilmu Ekonomi (STIE) dan Sekolah Tinggi Teknik (STT) 
-    sesuai dengan Surat Keputusan Menteri Pendidikan Nasional Republik  Indonesia Nomor : 664/KPTI/I/2019 tanggal 2 Agustus 2019.
-    UPB berdiri pada hamparan lahan seluas 11.603 m2yang beralamatkan 
-    di Jalan Inpeksi Kalimalang Tegal Danas Arah DELTAMAS, Cikarang Pusat – Kabupaten Bekasi,
-    yang mana berada di tengah-tengah Pusat Segitiga Emas LIPPO Cikarang/ Meikarta, Jababeka, 
-    Delta Mas yang dikelilingi oleh Kawasan Industri terbesar di Asia Tenggara</i></p3>
-    
+    <section id="introduce">
+        <div class="row">
+            <img src="foto saya" title="M.Iqbal Maulana" alt="M.Iqbal Maulana" class="image-circle" width="230"
+            style="float: left; border: 2px solid black;">
+            <h1>Hello!</h1>
+            <p> Nama saya M.Iqbal Maulana dan sering dipanggil iqbal. Saya lahir di <i>Kebumen</i> pada tanggal 08 November 2000 
+          sebagai anak pertama dari 3 bersaudara. Saat ini, saya tinggal bersama orang tua dan keluarga di <i>Bekasi</i>.</p>
+        </div>
+    </section>
     <?= $this->include('template/footer'); ?>
 </body>
 </html>
